c7218ccf849f21181628e87bc8929ce7f6b3997e
aleksandartsts <alexconev8i@gmail.com>
2023-02-13 09:40:40 +0200

    Rook begining

--- src/main/java/edu/school/chess/logic/Rook.java
+++ src/main/java/edu/school/chess/logic/Rook.java
@@ -3,8 +3,16 @@ package edu.school.chess.logic;
 import java.util.List;
 
 public class Rook extends Figure{
-    @Override
-    public boolean move(int row, int column, List<Figure> figureList) {
-        return false;
+
+    public Rook(int row, int column,boolean figureColor, boolean selected){
+        super(figureColor, selected,row,column);
+    }
+
+    public boolean move(int row, int column, List<Figure> figureList)  {
+        if(!isSelected()){
+            throw new Exception("The figure must be selected");
+        }else if{
+
+        }
     }
 }
