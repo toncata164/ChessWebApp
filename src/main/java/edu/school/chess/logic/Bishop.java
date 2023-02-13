4013f2bb7a14b1e3c8e0bb220e9c962afb7b7c8a
Hamumu11 <kalohaker@gmail.com>
2023-02-13 09:40:24 +0200

    a

--- src/main/java/edu/school/chess/logic/Bishop.java
+++ src/main/java/edu/school/chess/logic/Bishop.java
@@ -3,8 +3,13 @@ package edu.school.chess.logic;
 import java.util.List;
 
 public class Bishop extends Figure{
+    public Bishop(boolean color, boolean selected, int row, int column) {
+        super(color, selected, row, column);
+    }
+
     @Override
     public boolean move(int row, int column, List<Figure> figureList) {
+
         return false;
     }
 }
