cb645c31b30933b1c7127ccbc3672f31b8107adb
dimitartsm <dimitarmladenov21@gmail.com>
2023-02-13 09:40:43 +0200

    knight class

--- src/main/java/edu/school/chess/logic/Knight.java
+++ src/main/java/edu/school/chess/logic/Knight.java
@@ -3,8 +3,23 @@ package edu.school.chess.logic;
 import java.util.List;
 
 public class Knight extends Figure{
-    @Override
+    public Knight(boolean color, boolean selected, int row, int column) {
+        super(color, selected, row, column);
+    }
+
+
+
     public boolean move(int row, int column, List<Figure> figureList) {
         return false;
+    }
+    if(isSelected() == false)
+    {
+        throw new Exception("You need to select the figure first!");
+    }
+    else if
+
+
+    {
+        throw new Exception("The knight can't move there!");
     }
 }
