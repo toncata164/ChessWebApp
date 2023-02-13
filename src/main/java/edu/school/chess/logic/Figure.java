41b83fef6cddbd205a336b4baf82b8ddc8a2c10f
aleksandarsi1 <aleksandarsi1@students.npmg.org>
2023-02-13 09:18:32 +0200

    Figure class

--- src/main/java/edu/school/chess/logic/Figure.java
+++ src/main/java/edu/school/chess/logic/Figure.java
@@ -3,9 +3,48 @@ package edu.school.chess.logic;
 import java.util.List;
 
 public abstract class Figure {
-    //color
-    //selected
-    //row
-    //column
+    public boolean figureColor;
+    public boolean selected;
+    public int row;
+    public int column;
+
+    public void setColor(boolean newColor){
+        this.figureColor=newColor;
+    }
+    public void setSelected(boolean newSelected){
+        this.selected=newSelected;
+    }
+    public void setRow(int newRow){
+        this.row=newRow;
+    }
+    public void setColumn(int newColumn){
+        this.column=newColumn;
+    }
+    public boolean getColor(){
+        return figureColor;
+    }
+    public boolean isSelected(){
+        return selected;
+    }
+    public int getRow(){
+        return row;
+    }
+    public int getColumn(){
+        return column;
+    }
+    public Figure(boolean color, boolean selected, int row, int column){
+        setColor(color);
+        setSelected(selected);
+        setRow(row);
+        setColumn(column);
+    }
+    public String toString(){
+        if(isSelected()==true){
+            return "F";
+        }
+        else{
+            return "f";
+        }
+    }
     public abstract boolean move(int row, int column, List<Figure> figureList);
 }
