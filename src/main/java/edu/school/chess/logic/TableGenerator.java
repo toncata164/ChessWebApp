9a04a8b68bfef352ddd8ca41f6a8c065c05d3479
Toncata <a.v.denev@gmail.com>
2023-02-14 08:32:26 +0200

    table generator ready

--- src/main/java/edu/school/chess/logic/TableGenerator.java
+++ src/main/java/edu/school/chess/logic/TableGenerator.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 public class TableGenerator {
     public static Figure[][] generateTable(List<Figure> figures){
-
+        if(figures == null){
+            throw new IllegalArgumentException("List of figures cannot be null");
+        }
+        final Figure[][] table = new Figure[8][8];
+        figures.forEach(f->table[f.getRow()][f.getColumn()] = f);
+        return table;
     }
 }
