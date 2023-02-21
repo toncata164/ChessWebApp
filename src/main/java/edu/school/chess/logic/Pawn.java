67ad6a1d39614606aceb4e734bb69d9ef9d073f3
aleksandarsi1 <aleksandarsi1@students.npmg.org>
2023-02-21 09:45:23 +0200

    Pawn (unfinished)

--- src/main/java/edu/school/chess/logic/Pawn.java
+++ src/main/java/edu/school/chess/logic/Pawn.java
@@ -3,14 +3,45 @@ package edu.school.chess.logic;
 import java.util.List;
 
 public class Pawn extends Figure{
+    private boolean hasMovedBefore=false;
+
+    public Pawn(boolean color, boolean selected, int row, int column) {
+        super(color, selected, row, column);
+    }
+
     @Override
     public boolean move(int row, int column, List<Figure> figureList) {
+        int curRow=this.getRow();
+        int curCol=this.getColumn();
+        if(curCol==column && curRow-1==row){
+            return true;
+        }
+        if(hasMovedBefore==false){
+            if(curCol==column && curRow-2==row){
+                hasMovedBefore=true;
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     protected boolean canMove(int row, int column, List<Figure> figureList) {
-        // TODO Auto-generated method stub
+        if(move(row,column, figureList)==true){
+            setRow(row);
+            setColumn(column);
+            return true;
+        }
+        for(int i=0; i<figureList.size(); i++){
+            if(figureList.get(i)!=this && figureList.get(i).getColor()!=this.getColor()){
+                if(figureList.get(i).getRow()==row-1 && (figureList.get(i).getColumn()==column-1 || figureList.get(i).getColumn()==column+1)){
+                    setRow(row);
+                    setColumn(column);
+                    figureList.remove(i);
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
